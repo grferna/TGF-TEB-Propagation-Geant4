6fc1ff864939ed1746f72d78f7e307c8c58624b8
David Sarria <david.sarria@uib.no>
2019-05-13 17:21:37 +0200

    Update compile_Windows.bat

--- compile_Windows.bat
+++ compile_Windows.bat
@@ -13,7 +13,7 @@ if exist CMakeCache.txt del CMakeCache.txt
 %G4_bat_file_dir%\cmake_win32\bin\cmake.exe ^
 -DCMAKE_CONFIGURATION_TYPES=%build_type% ^
 -DCMAKE_PREFIX_PATH=%G4_bat_file_dir%\install\lib\Geant4-10.4.3 ^
-%G4_bat_file_dir%
+%G4_bat_file_dir%/src/
 
 %G4_bat_file_dir%\cmake_win32\bin\cmake.exe --build . --config %build_type%
 
